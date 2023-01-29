5d7d306c5dab1b0dc8138505bcc9242ef2a52f19
Gleb Rukhovich <rukhovich@gmail.com>
2023-01-29 17:48:34 +0100

    Added docker compose, app and prometheus.yml

---
+++ app.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request
+from flask.json import jsonify
+from flask_cors import cross_origin
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import cross_origin, CORS
+from datetime import datetime
+from uuid import uuid4
+from PIL import Image
+
+from coffeegame import CoffeeGame
+import matplotlib
+
+matplotlib.use('Agg')
+
+app = Flask(__name__, static_folder='static')
+# app.config['SQLALCHEMY_DATABESE_URI'] = "sqlite:///coffee_game.sqlite3"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:299792458@pgdb:5432/coffee_game"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:299792458@localhost:5432/coffee_game"
+
+cors = CORS(app)
+# metrics = PrometheusMetrics(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+url = "localhost:5000/upload"
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary key
+    players = db.Column(db.JSON, nullable=False)
+    uuid = db.Column(db.String, nullable=False, unique=True)
+    pdf = db.Column(db.String, nullable=False, unique=True)
+    random_state = db.Column(db.Integer, nullable=False, default=42)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    orientation = db.Column(db.String, nullable=False, default='pointy')
+    grid_size = db.Column(db.Float, nullable=False, default=5)
+
+    images = db.relationship("Image", backref='game', lazy=True)
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary key
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    image = db.Column(db.String)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    config = db.Column(db.JSON)
+
+
+@app.route("/create_game", methods=['POST'])
+@cross_origin()
+def create_game():
+    request_json = request.get_json()
+    print("kek")
+    players = request_json.get("players")
+    random_state = request_json.get('random_state', 42)
+    orientation = request_json.get('orientation', 'pointy')
+    grid_size = request_json.get('grid_size', 5)
+
+    uuid = uuid4().hex
+    cg = CoffeeGame(
+        players=players,
+        random_state=random_state,
+        orientation=orientation,
+        grid_size=grid_size,
+        uuid=uuid,
+        url=url
+    )
+    cg.generate_game_field(f"static/pdf/{uuid}.pdf")
+
+    game = Game(
+        players=players,
+        random_state=random_state,
+        orientation=orientation,
+        grid_size=grid_size,
+        uuid=uuid,
+        pdf=f"{uuid}.pdf"
+    )
+
+    db.session.add(game)
+    db.session.commit()
+
+    print(game.id)
+
+    return jsonify({
+        "id": game.id,
+        "url": f"localhost:5000/static/pdf/{game.pdf}",
+        "date": game.date
+    })
+
+
+@app.route("/upload_image", methods=['POST'])
+@cross_origin()
+def upload_image():
+    file = None
+    for f in request.files:
+        file = request.files[f]
+
+    # app.root_path, 'static/customlogos/logo.png'
+
+    if file:
+        img = Image.open(request.files['file'].stream)
+
+        return {
+            "results": "results"
+        }
